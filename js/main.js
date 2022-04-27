338e62acc336da1a433fed710085ea49162aa310
Muzaffarxon04 <muzaffarxonabdusalomov04@gmail.com>
2022-04-27 12:57:07 +0500

    inital commit

---
+++ js/main.js
@@ -0,0 +1,48 @@
+
+
+function range1(){
+    let SliderValue = document.getElementById("slider").value;
+    let  elMyImage = document.getElementById("slider__my-image");
+    elMyImage.style.clipPath = `polygon(0 0, ${SliderValue}% 0, ${SliderValue}% 100%, 0 100%)`;
+    console.log(elMyImage.style.cliath = "polygon(0 0, " + SliderValue + "% 0" + SliderValue + "% 100%, 0 100%)");
+}
+
+function range2(){
+    let SliderValue = document.getElementById("slider2").value;
+    let  elMyImage = document.getElementById("slider__my-image2");
+    elMyImage.style.clipPath = `polygon(0 0, ${SliderValue}% 0, ${SliderValue}% 100%, 0 100%)`;
+    console.log(elMyImage.style.cliath = "polygon(0 0, " + SliderValue + "% 0" + SliderValue + "% 100%, 0 100%)");
+}
+
+function range3(){
+    let SliderValue = document.getElementById("slider3").value;
+    let  elMyImage = document.getElementById("slider__my-image3");
+    elMyImage.style.clipPath = `polygon(0 0, ${SliderValue}% 0, ${SliderValue}% 100%, 0 100%)`;
+    console.log(elMyImage.style.cliath = "polygon(0 0, " + SliderValue + "% 0" + SliderValue + "% 100%, 0 100%)");
+}
+
+
+function range4(){
+    let SliderValue = document.getElementById("slider4").value;
+    let  elMyImage = document.getElementById("slider__my-image4");
+    elMyImage.style.clipPath = `polygon(0 0, ${SliderValue}% 0, ${SliderValue}% 100%, 0 100%)`;
+    console.log(elMyImage.style.cliath = "polygon(0 0, " + SliderValue + "% 0" + SliderValue + "% 100%, 0 100%)");
+}
+
+function range5(){
+    let SliderValue = document.getElementById("slider5").value;
+    let  elMyImage = document.getElementById("slider__my-image5");
+    elMyImage.style.clipPath = `polygon(0 0, ${SliderValue}% 0, ${SliderValue}% 100%, 0 100%)`;
+    console.log(elMyImage.style.cliath = "polygon(0 0, " + SliderValue + "% 0" + SliderValue + "% 100%, 0 100%)");
+}
+
+
+
+
+
+
+
+
+
+
+
